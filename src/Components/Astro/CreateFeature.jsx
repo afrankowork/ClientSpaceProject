db4012fff7fab9cd07baa6795620bbcd97edd747
ashtonsprunger <sprungerashton@gmail.com>
2020-11-02 17:21:55 -0500

    wrong credentials checker

--- src/Components/Astro/CreateFeature.jsx
+++ src/Components/Astro/CreateFeature.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
+import React from "react";
 
-return CreateFeature(
-
-);
+const CreateFeature = () => {};
 
 export default CreateFeature;
--- src/Components/Astro/UpdateFeature.jsx
+++ src/Components/Astro/UpdateFeature.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
+import React from "react";
 
-return UpdateFeature(
-
-);
+const UpdateFeature = () => {};
 
 export default UpdateFeature;
