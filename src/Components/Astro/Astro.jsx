abbb024d1983705a36d1b0d03e97c2cbbe117fe6
Jonathan Ingersoll <jonathan.ingersoll@gmail.com>
2020-11-04 09:45:56 -0500

    Built out ViewAllFeatures; Slight edits to Astro.jsx

--- src/Components/Astro/Astro.jsx
+++ src/Components/Astro/Astro.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button } from "reactstrap";
+import ViewAllFeatures from './ViewAllFeatures';
 // import stars from "./stars";
 
 export default function Astro(props) {
@@ -11,6 +12,7 @@ export default function Astro(props) {
           Nasa Photo of the Day
         </Button>
       </div>
+      <ViewAllFeatures token={props.token} />
     </>
   );
 }
