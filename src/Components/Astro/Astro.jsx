1a145768ad68cd4a28f9502e67e8f1292751e403
ashtonsprunger <sprungerashton@gmail.com>
2020-11-04 09:09:31 -0500

    delete component

--- src/Components/Astro/Astro.jsx
+++ src/Components/Astro/Astro.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 
-import { Button } from 'reactstrap';
+import { Button } from "reactstrap";
 //import stars from "./stars";
 
+import DeleteFeature from "./DeleteFeature";
 
 export default function Astro(props) {
   return (
     <>
-    <div className="astro">
-      <Button id="nasabutton" onClick={() => props.changeView('nasa-photo')}>Nasa Photo of the Day</Button>
-    </div>
-    <div>
-      <Button id="picturelog" onClick={() => props.changeView('output', 'imageTitle', 'progress-bar')}>Your Photos</Button>
+      <div className="astro">
+        <Button id="nasabutton" onClick={() => props.changeView("nasa-photo")}>
+          Nasa Photo of the Day
+        </Button>
       </div>
+      <div>
+        <Button
+          id="picturelog"
+          onClick={() =>
+            props.changeView("output", "imageTitle", "progress-bar")
+          }
+        >
+          Your Photos
+        </Button>
+      </div>
+      <DeleteFeature token={props.token} id={6} />
     </>
   );
 }
